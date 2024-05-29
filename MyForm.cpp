cda06585db24e04a6eb2e95b047d00f8446fecb4
USER <USER@ZEMPAIII>
2024-05-29 10:39:19 +0800

    Add project files.

---
+++ MyForm.cpp
@@ -0,0 +1,13 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Project::MyForm form;
+	Application::Run(% form);
+
+
+}
