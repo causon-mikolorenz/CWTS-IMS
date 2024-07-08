81fc462e7429ad11b56f5ed2839a8f05fbfa9f1c
aaron efondo <aefondo124@gmail.com>
2024-07-08 19:33:55 +0800

    change "MyForm.cpp" to "main.cpp"

--- main.cpp
+++ main.cpp
@@ -1,2 +1,11 @@
-#include "MyForm.h"
+#include "LoginWindow.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Project::LoginWindow form;
+	Application::Run(% form);
+}
